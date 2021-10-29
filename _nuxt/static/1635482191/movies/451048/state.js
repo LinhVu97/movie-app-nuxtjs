0d702cc5f298b138cb0069f16193c746b1dee958
LinhVu97 <linhvn2303@gmail.com>
2021-10-29 11:36:41 +0700

    Updates

---
+++ _nuxt/static/1635482191/movies/451048/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmovie-app-nuxtjs\u002F_nuxt\u002Fstatic\u002F1635482191",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovies\u002F451048",config:{_app:{basePath:"\u002Fmovie-app-nuxtjs\u002F",assetsPath:"\u002Fmovie-app-nuxtjs\u002F_nuxt\u002F",cdnURL:a}}}}(null));
